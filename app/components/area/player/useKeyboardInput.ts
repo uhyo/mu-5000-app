408cbe2e3198cd85cc047a1f209df174a2bc677f
uhyo <uhyo@uhy.ooo>
2021-12-30 16:37:31 +0900

    support for instant key input

--- app/components/area/player/useKeyboardInput.ts
+++ app/components/area/player/useKeyboardInput.ts
@@ -3,16 +3,35 @@ import { useMainLoopCallback } from "../mainloop/useMainLoopCallback";
 
 export type KeyType = "ArrowUp" | "ArrowDown" | "ArrowLeft" | "ArrowRight";
 
+type InstantKeyState = {
+  /**
+   * 0 means not used.
+   * 1 means pressed within last frame.
+   * 2 means released within the same frame.
+   */
+  state: 0 | 1 | 2;
+  keyType: KeyType;
+};
+
 type UseKeyboardInputInput = {
   onKeyInput: (key: KeyType) => void;
 };
 
 export function useKeyboardInput({ onKeyInput }: UseKeyboardInputInput) {
+  /**
+   * Stack of keys that are currently pressed.
+   * (last one is effective)
+   */
   const keyStack = useRef<KeyType[]>([]);
+  const instantKey = useRef<InstantKeyState>({ state: 0, keyType: "ArrowUp" });
+
   useMainLoopCallback(() => {
-    if (keyStack.current.length > 0) {
+    if (instantKey.current.state === 2) {
+      onKeyInput(instantKey.current.keyType);
+    } else if (keyStack.current.length > 0) {
       onKeyInput(keyStack.current[keyStack.current.length - 1]);
     }
+    instantKey.current.state = 0;
   }, [onKeyInput]);
 
   useEffect(() => {
@@ -35,6 +54,8 @@ export function useKeyboardInput({ onKeyInput }: UseKeyboardInputInput) {
           ) {
             keyStack.current.push(event.key);
           }
+          instantKey.current.state = 1;
+          instantKey.current.keyType = event.key;
           break;
         default:
           break;
@@ -42,6 +63,12 @@ export function useKeyboardInput({ onKeyInput }: UseKeyboardInputInput) {
     }
     function keyupHandler(event: KeyboardEvent) {
       keyStack.current = keyStack.current.filter((key) => key !== event.key);
+      if (
+        instantKey.current.state === 1 &&
+        instantKey.current.keyType === event.key
+      ) {
+        instantKey.current.state = 2;
+      }
     }
   }, [onKeyInput]);
 }
